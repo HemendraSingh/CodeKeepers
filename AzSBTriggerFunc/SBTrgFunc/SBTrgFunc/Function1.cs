e3e2a41b40d4c9ba622bccc03a196d60e3992420
Hemendra Singh <admin@hemendrasingh.com>
2019-04-28 11:10:04 +0530

    Update Function1.cs

--- AzSBTriggerFunc/SBTrgFunc/SBTrgFunc/Function1.cs
+++ AzSBTriggerFunc/SBTrgFunc/SBTrgFunc/Function1.cs
@@ -1,6 +1,9 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Data.SqlClient;
 
 namespace SBTrgFunc
 {
@@ -9,7 +12,30 @@ namespace SBTrgFunc
         [FunctionName("Function1")]
         public static void Run([ServiceBusTrigger("queue1", AccessRights.Manage, Connection = "SBConn")]string myQueueItem, TraceWriter log)
         {
-            log.Info($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            var json_obj = JsonConvert.DeserializeObject<UserAnswer>(myQueueItem);
+
+            SqlConnection conn = new SqlConnection("Initial Catalog=HackathonDB;Data Source= hackathondbsrv.database.windows.net;User ID=Hackathon;Password=Danske@123");
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = "insert into QuizAnswers(UserQuizId,QuestionId,Answer) values(" + json_obj.UserQuestionId + "," + json_obj.QuestionId + ",'" + json_obj.Answer + "')";
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                conn.Close();
+            }        
+        }
+        
+        class UserAnswer
+        { 
+            public int UserQuestionId { get; set; }
+            public int QuestionId { get; set; }
+            public string Answer { get; set; }
         }
     }
 }
