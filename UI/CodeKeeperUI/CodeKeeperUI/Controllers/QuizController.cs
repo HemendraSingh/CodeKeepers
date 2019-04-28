8d9448d0bf2d96f0b231aa0c4420edfa6c589a59
Sundar <psundar001@gmail.com>
2019-04-28 10:15:15 +0530

    Service bus integration

---
+++ UI/CodeKeeperUI/CodeKeeperUI/Controllers/QuizController.cs
@@ -0,0 +1,41 @@
+﻿using CodeKeeperUI.Helpers;
+using CodeKeeperUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace CodeKeeperUI.Controllers
+{
+    public class QuizController : Controller
+    {
+        // GET: Quiz
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult SubmitAnswer(UserAnswer userAnswer)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonData = js.Serialize(userAnswer);
+            ServiceBusHelper.MainAsync(jsonData);
+            return new JsonResult()
+            {
+                Data = true
+            };
+        }
+    }
+
+    public class UserAnswer
+    {
+        public int UserQuizId { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
---
+++ UI/CodeKeeperUI/CodeKeeperUI/Helpers/ServiceBusHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CodeKeeperUI.Helpers
+{
+    public class ServiceBusHelper
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://hackathonsrvbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Aq0ZO/GcZ6lj7TjF9FmiAy0gWkcYHqni4xNvzQyECxo=";
+        const string QueueName = "queue1";
+        static IQueueClient queueClient;
+        
+        public static async Task MainAsync(string messageBody)
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            await SendMessagesAsync(messageBody);
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(string messageBody)
+        {
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            // Write the body of the message to the console.
+            Console.WriteLine($"Sending message: {messageBody}");
+
+            // Send the message to the queue.
+            await queueClient.SendAsync(message);
+        }
+    }
+}
